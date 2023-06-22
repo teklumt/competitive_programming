078d96ac3c48555e0cda067b615f95d12b27f501
Teklu moges <135549662+teklumt@users.noreply.github.com>
2023-06-22 22:25:05 +0300

    Time: 95 ms (7.71%), Space: 16.9 MB (64.12%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        num=["0","1","2","3","4","5","6","7","8","9"]
+        new=""
+        bol=True
+        for n in s:
+            if n.isalpha():
+                new+=n.lower()
+            if n in num:
+                new+=n
+        for n in range(len(new)):
+            if new[n]!=new[-1-n]:
+                bol=False
+                break
+
+        return bol
