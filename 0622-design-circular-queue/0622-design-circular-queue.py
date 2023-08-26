4e040500ab216e28e154f0bfc7c222ac3e6fb668
Teklu moges <135549662+teklumt@users.noreply.github.com>
2023-08-26 23:17:19 +0300

    Time: 61 ms (95.73%), Space: 16.8 MB (85.10%) - LeetHub

---
+++ 0622-design-circular-queue/0622-design-circular-queue.py
@@ -0,0 +1,45 @@
+class MyCircularQueue:
+
+    def __init__(self, k: int):
+        self.data=[]
+        self._len=k  
+
+    def enQueue(self, value: int) -> bool:
+        if len(self.data)<self._len:
+            self.data.append(value)
+            return True
+
+    def deQueue(self) -> bool:
+        if len(self.data)!=0:
+            self.data.remove(self.data[0])
+            return True
+
+    def Front(self) -> int:
+        if len(self.data)==0:
+            return -1
+        else:
+            return self.data[0]        
+
+
+    def Rear(self) -> int:
+        if len(self.data)==0:
+            return -1
+        else:
+            return self.data[-1]              
+
+
+    def isEmpty(self) -> bool:
+        return len(self.data)==0
+
+    def isFull(self) -> bool:
+        return len(self.data)==self._len    
+
+
+# Your MyCircularQueue object will be instantiated and called as such:
+# obj = MyCircularQueue(k)
+# param_1 = obj.enQueue(value)
+# param_2 = obj.deQueue()
+# param_3 = obj.Front()
+# param_4 = obj.Rear()
+# param_5 = obj.isEmpty()
+# param_6 = obj.isFull()
