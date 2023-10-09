e5fdfcd9cc869e46f1db10b453593e720608ac97
Teklu moges <135549662+teklumt@users.noreply.github.com>
2023-10-09 23:49:52 +0300

    Time: 31 ms (97.17%), Space: 16.3 MB (54.87%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,4 +3,13 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        nums.sort()
+        result=[]
+        num=[0]*(max(nums)+1)
+        for i in nums:
+            num[i]+=1
+        for i in range(len(num)):
+            if num[i]!=0:
+                result+=[i]*num[i]
+        for i in range(len(nums)):
+            nums[i]=result[i]
+     
