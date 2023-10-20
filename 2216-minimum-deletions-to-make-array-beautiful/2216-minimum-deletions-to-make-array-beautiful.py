8dd53c79a2a3fb78294b408adad08021417a82ac
Teklu moges <135549662+teklumt@users.noreply.github.com>
2023-10-20 23:48:00 +0300

    Time: 1108 ms (52.01%), Space: 30.4 MB (84.23%) - LeetHub

--- 2216-minimum-deletions-to-make-array-beautiful/2216-minimum-deletions-to-make-array-beautiful.py
+++ 2216-minimum-deletions-to-make-array-beautiful/2216-minimum-deletions-to-make-array-beautiful.py
@@ -1,9 +1,11 @@
 class Solution:
     def minDeletion(self, nums: List[int]) -> int:
         stack=[]
+        count=0
         for i in nums:
             if stack and len(stack)%2!=0 and i==stack[-1]:
-                continue
+                count+=1
             else:
                 stack.append(i)
-        return len(nums)-len(stack) if len(stack)%2==0 else len(nums)-len(stack)+1
+        
+        return count if len(stack)%2==0 else count+1
