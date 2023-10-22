d75ab2e5fba83f2f05a577cd3947e1583672a42a
Teklu moges <135549662+teklumt@users.noreply.github.com>
2023-10-22 10:28:51 +0300

    Time: 833 ms (8.96%), Space: 31.2 MB (98.11%) - LeetHub

--- 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.py
+++ 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.py
@@ -10,13 +10,23 @@ class Solution:
             if i==0:
                 presum=0
                 postsum-=nums[i]
+                if nums[i] not in myhash:
+                    myhash[nums[i]].append(presum)
+                    myhash[nums[i]].append(postsum)
+                    myhash[nums[i]].append(i-0)
+                    myhash[nums[i]].append(n-i-1)
             elif i==n-1:
                 presum+=nums[i-1]
                 postsum=0
+                if nums[i] not in myhash:
+                    myhash[nums[i]].append(presum)
+                    myhash[nums[i]].append(postsum)
+                    myhash[nums[i]].append(i-0)
+                    myhash[nums[i]].append(n-i-1)
             else:
                 presum+=nums[i-1]
                 postsum-=nums[i]
-            if nums[i] not in myhash:
+                if nums[i] not in myhash:
                     myhash[nums[i]].append(presum)
                     myhash[nums[i]].append(postsum)
                     myhash[nums[i]].append(i-0)
