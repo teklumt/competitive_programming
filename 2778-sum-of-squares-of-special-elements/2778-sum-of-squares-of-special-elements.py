e72490a9729178d28cd655ac384419763bc358d5
Teklu moges <135549662+teklumt@users.noreply.github.com>
2023-10-24 13:32:00 +0300

    Time: 64 ms (74.88%), Space: 16 MB (95.06%) - LeetHub

--- 2778-sum-of-squares-of-special-elements/2778-sum-of-squares-of-special-elements.py
+++ 2778-sum-of-squares-of-special-elements/2778-sum-of-squares-of-special-elements.py
@@ -2,7 +2,10 @@ class Solution:
     def sumOfSquares(self, nums: List[int]) -> int:
         res=0
         n=len(nums)
-        for i in range(1,len(nums)+1):
+        myhash={}
+        for i in range(len(nums)):
+            myhash[i]=nums[i]
+        for i in range(1,n+1):
             if n%i==0:
-                res+=nums[i-1]*nums[i-1]
+                res+=myhash[i-1]*myhash[i-1]
         return res
