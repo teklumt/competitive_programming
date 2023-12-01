89b2d82cce9cc326e7482cd09c7ac37c3564fb12
Teklu moges <135549662+teklumt@users.noreply.github.com>
2023-12-01 17:37:58 +0300

    Added solution for Roman to Integer

--- week1/roman-to-integer.py
+++ week1/roman-to-integer.py
@@ -6,15 +6,14 @@ class Solution:
             "I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000,"IV":4,"IX":9,
             "XL":40,"XC":90,"CD":400,"CM":900
         }
-        check=["IV","IX","XL","XC","CD","CM"]
-        ori=["I","V","X","L","C","D","M"]
+        
         for n in range(0,len(s)):
             if n in store:
                 continue
-            elif s[n:n+2] in check:
+            elif s[n:n+2] in dic:
                 result=result+ dic.get(s[n:n+2])
                 store.append(n+1)        
-            elif s[n] in ori:
+            elif s[n] in dic:
                 result=result+dic.get(s[n])
         return result
     
