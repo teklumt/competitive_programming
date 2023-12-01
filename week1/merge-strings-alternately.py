92e9943bd29295074b9e4668ed574d1a6703299f
Teklu moges <135549662+teklumt@users.noreply.github.com>
2023-12-01 18:26:57 +0300

    Added solution for Merge Strings Alternately

---
+++ week1/merge-strings-alternately.py
@@ -0,0 +1,9 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        res=""
+        for i in range(max(len(word1),len(word2))):
+            if i <len(word1):
+                res+=word1[i]
+            if i<len(word2):
+                res+=word2[i]
+        return res
