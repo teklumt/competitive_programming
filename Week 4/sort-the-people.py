b658a40e234123f5423dc7d57ad11b90bcbd6849
Teklu moges <135549662+teklumt@users.noreply.github.com>
2023-12-26 19:48:56 +0300

    Added solution for Sort the People

---
+++ Week 4/sort-the-people.py
@@ -0,0 +1,4 @@
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        return [ j[1] for j in sorted([[heights[i],names[i]] for i in range(len(names))])[::-1]]
+
