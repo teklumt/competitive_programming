79a451aad28aa08b205318e5fafe0ffb0cf53797
Teklu moges <135549662+teklumt@users.noreply.github.com>
2023-12-31 09:41:23 +0300

    Time: 1198 ms (5.88%), Space: 26 MB (5.88%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.py
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.py
@@ -1,5 +1,10 @@
-class Solution:
-    def maxIceCream(self, costs: List[int], coins: int) -> int:
+class Solution(object):
+    def maxIceCream(self, costs, coins):
+        """
+        :type costs: List[int]
+        :type coins: int
+        :rtype: int
+        """
         counting=[0]*(max(costs)+1)
         output=[]
         for i in costs:
@@ -12,3 +17,4 @@ class Solution:
             else:
                 return i
         return len(output)
+        
