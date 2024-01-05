8005ef7fe5a45e0a7d8f7f6644427b15a994d39d
Teklu moges <135549662+teklumt@users.noreply.github.com>
2024-01-05 20:15:53 +0300

    Added solution for Circular Array Loop

--- Week 5/circular-array-loop.py
+++ Week 5/circular-array-loop.py
@@ -8,23 +8,14 @@ class Solution:
                 curr=(j+nums[j])%len(nums)
                 if j==i:
                     seen.add(i)
-                    if curr==j:
-                        break
-                    
+                if curr==j:
+                    break
+                elif curr in seen:
+                    return True
+                else:
                     if nums[curr]*nums[j]<0:
                         break
                     else:
                         j=curr
                         seen.add(j)
-                else:
-                    if curr==j:
-                        break
-                    if curr in seen:
-                        return True
-                    else:
-                        if nums[curr]*nums[j]<0:
-                            break
-                        else:
-                            j=curr
-                            seen.add(j)
         return False
