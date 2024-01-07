f8a3c3fb48cb13af3afbdba11c8cbee341a51e19
Teklu moges <135549662+teklumt@users.noreply.github.com>
2024-01-07 14:54:32 +0300

    Time: 44 ms (37.83%), Space: 17.4 MB (10.24%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,13 +3,32 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        result=[]
-        num=[0]*(max(nums)+1)
-        for i in nums:
-            num[i]+=1
-        for i in range(len(num)):
-            if num[i]!=0:
-                result+=[i]*num[i]
-        for i in range(len(nums)):
-            nums[i]=result[i]
+        ptr1=0
+        ptr2=0
+        ptr3=len(nums)-1
+        while ptr1<=ptr3:
+            if nums[ptr1]==0:
+                nums[ptr2],nums[ptr1]=nums[ptr1],nums[ptr2]
+                ptr2+=1
+                ptr1+=1
+                
+            elif nums[ptr1]==2:
+                nums[ptr3],nums[ptr1]=nums[ptr1],nums[ptr3]
+                ptr3-=1
+            else:
+                ptr1+=1
+                
+                
+                
+                
+# #         Method 2
+#         result=[]
+#         num=[0]*(max(nums)+1)
+#         for i in nums:
+#             num[i]+=1
+#         for i in range(len(num)):
+#             if num[i]!=0:
+#                 result+=[i]*num[i]
+#         for i in range(len(nums)):
+#             nums[i]=result[i]
      
