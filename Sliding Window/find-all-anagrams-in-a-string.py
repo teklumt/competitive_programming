6e6a0784219a7684646062006daeb476fecf2464
Teklu moges <135549662+teklumt@users.noreply.github.com>
2024-01-11 17:17:21 +0300

    Added solution for Find All Anagrams in a String

---
+++ Sliding Window/find-all-anagrams-in-a-string.py
@@ -0,0 +1,156 @@
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+              
+        if len(s)<len(p):
+            return []
+        num=0
+        res=[]
+        for i in p:
+            num+=hash(i)
+        
+        summ=0
+        j=0
+        for i in range(len(p)):
+            summ+=hash(s[i])
+        if num==summ:
+            res.append(j)
+        
+        
+        for i in range(len(p),len(s)):
+            summ-=hash(s[j])
+            summ+=hash(s[i])
+            j+=1
+            if summ==num:
+                res.append(j)
+        
+        return res
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+
+
+
+
+
+            
+        
+        # if win:
+        #     temp=Counter(win)
+        #     if temp==str1:
+        #         res.append(j)
+        #     win.popleft()
+        #     win.append(s[i])
+        #     j+=1
+        # return res
+
+
+
+
+
+        #         bol=True
+        #         for k in str1:
+        #             if str1[k]!=temp[k]:
+        #                 bo=False
+        #                 break
+        #         if bol:
+        #             res.append(j)
+        #         win.popleft()
+        #         win.append(s[i])
+        #         j+=1
+        # # if win:
+        # #     temp=Counter(win)
+        # #     bol=True
+        # #     for k in str1:
+        # #         if str1[k]!=temp[k]:
+        # #             bo=False
+        # #             break
+        # #     if bol:
+        # #         res.append(j)
+        # #     win.popleft()
+        # #     win.append(s[i])
+        # return res
+                
+
+
+
+
+
+
+
+
+        n=len(p)
+        str1=sorted(list(p))
+        str2=deque(list(s[:n]))
+        res=[]
+        left=1
+        if str1==sorted(str2):
+            res.append(0)
+        for i in range(n,len(s)):
+            str2.popleft()
+            str2.append(s[i])
+            if str1==sorted(str2):
+                res.append(left)
+            left+=1
+        return res
