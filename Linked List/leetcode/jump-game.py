90597f3222dac05cb690be0dfb48330ad7898a1e
Teklu moges <135549662+teklumt@users.noreply.github.com>
2024-02-22 09:18:57 +0300

    Add solution for Jump Game

--- Linked List/leetcode/jump-game.py
+++ Linked List/leetcode/jump-game.py
@@ -12,27 +12,21 @@ class Solution:
 
 
 
-
-
-
-
-
-
-
-        stack=['T']
-        for i in range(len(nums)-2,-1,-1):
-            if nums[i]==0:
-                stack.append('F')
-            elif nums[i]==1:
-                if stack[-1]=='T':
-                    stack.append('T')
-                else:
-                    stack.append('F')
-            else:
-                # new=stack.copy()
-                # new.reverse()
-                if 'T' in stack[::-1][:nums[i]]:
-                    stack.append('T')
-                else:
-                    stack.append('F')
-        return True if stack[-1]=='T' else False
+        # Method II
+        # stack=['T']
+        # for i in range(len(nums)-2,-1,-1):
+        #     if nums[i]==0:
+        #         stack.append('F')
+        #     elif nums[i]==1:
+        #         if stack[-1]=='T':
+        #             stack.append('T')
+        #         else:
+        #             stack.append('F')
+        #     else:
+        #         # new=stack.copy()
+        #         # new.reverse()
+        #         if 'T' in stack[::-1][:nums[i]]:
+        #             stack.append('T')
+        #         else:
+        #             stack.append('F')
+        # return True if stack[-1]=='T' else False
