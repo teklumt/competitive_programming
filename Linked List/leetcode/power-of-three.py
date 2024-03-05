2715cbc4f26304292083c32b11ca136919497360
Teklu moges <135549662+teklumt@users.noreply.github.com>
2024-03-06 00:47:47 +0300

    Add solution for Power of Three

--- Linked List/leetcode/power-of-three.py
+++ Linked List/leetcode/power-of-three.py
@@ -1,9 +1,9 @@
 class Solution:
     def isPowerOfThree(self, n: int) -> bool:
-        def isFour(n):
+        def isThree(n):
             if n == 1:
                 return True
             if n == 0:
                 return False
-            return isFour(n/3)
-        return isFour(n)
+            return isThree(n/3)
+        return isThree(n)
