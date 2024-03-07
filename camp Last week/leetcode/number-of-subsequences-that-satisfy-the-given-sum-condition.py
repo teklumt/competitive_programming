b1f5a9996180f073b5f7a203d29b09aeaea81ef2
Teklu moges <135549662+teklumt@users.noreply.github.com>
2024-03-07 15:50:04 +0300

    Add solution for Number of Subsequences That Satisfy the Given Sum Condition

--- camp Last week/leetcode/number-of-subsequences-that-satisfy-the-given-sum-condition.py
+++ camp Last week/leetcode/number-of-subsequences-that-satisfy-the-given-sum-condition.py
@@ -4,11 +4,12 @@ class Solution:
         res = 0
 
         nums.sort()
+
         
         for i in range(len(nums)):
             ter = target - nums[i]
             ind = (bisect_right(nums, ter) - 1) - i
             if ind >= 0:
-                res += 2**(ind) % mod
+                res += pow(2, ind, mod)
         return res % mod
 
