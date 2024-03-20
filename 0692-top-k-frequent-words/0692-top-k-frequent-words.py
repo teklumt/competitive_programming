43a6f32f84b5fd3081263fcb492cb6a849d346b2
Teklu moges <135549662+teklumt@users.noreply.github.com>
2024-03-20 21:11:24 +0300

    Time: 62 ms (31.65%), Space: 16.7 MB (32.52%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.py
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -1,30 +1,12 @@
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        result=[]
-        red=[]
-        for n in range(len(words)):
-            check=[]
-            count=1
-            if words[n] in red:
-                continue
-            else:
-                check.append(words[n])
-                red.append(words[n])
-                for m in range(n+1,len(words)):
-                        if words[m] in check:
-                            count+=1
-                check.append(count)
-                result.append(check)
-        result.sort()
-        for n in result:
-            n.reverse()
-        result.sort()
-        for n in range(len(result)):
-            for m in range(n+1,len(result)):
-                if result[n][0]==result[m][0]:
-                    if result[n][1]<result[m][1]:
-                            result[n],result[m]=result[m],result[n]     
-        final=[]
-        for m in range(k):
-            final.append(result[-1-m][1])
-        return final
+        count = Counter(words)
+        maxx = max(count.values())
+        res = [set() for _ in range(maxx)]
+        for i in words:
+            res[count[i] - 1].add(i)
+        res = res[::-1]
+        num = []
+        for i in range(len(res)):
+            num += sorted(res[i])
+        return num[:k]
