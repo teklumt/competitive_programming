83eb5a3dc746e4864b03394f0bdc3a47b29e036f
Teklu moges <135549662+teklumt@users.noreply.github.com>
2024-03-22 19:03:31 +0300

    Time: 107 ms (78.18%), Space: 18.2 MB (10.55%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,10 +1,11 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         result=[]
+        check= set(nums)
         if 0 not in nums:
                return 0
         for n in nums:
-            if n+1 not in nums:
+            if n+1 not in check:
                 result.append(n+1)
                 
         return min(result)
