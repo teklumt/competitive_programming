96e4c1bbad5b0835791676f960ed2d3ae044eb12
Teklu moges <135549662+teklumt@users.noreply.github.com>
2024-03-24 20:06:40 +0300

    Time: 1975 ms (48.21%), Space: 46.3 MB (18.13%) - LeetHub

--- 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.py
+++ 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.py
@@ -1,9 +1,33 @@
-from sortedcontainers import SortedList
 class Solution:
     def countSmaller(self, nums: List[int]) -> List[int]:
-        res = []
-        num = SortedList(nums)
-        for i in range(len(nums)):
-            res.append(bisect_left(num,nums[i]))
-            num.remove(nums[i])
-        return res  
+        newNum = [[nums[i], i] for i in range(len(nums))]
+
+        res = [0] * len(nums)
+        def  marge(left, right):
+           
+            i = 0
+            j = 0
+            while i < len(left):
+
+                while j < len(right) and right[j][0] < left[i][0]:
+                    
+                    j+=1
+                res[left[i][1]] +=  j
+                i += 1
+
+            
+            return sorted(left + right)
+        
+
+        def margeSort(newNum ):
+            if len(newNum) <= 1:
+                return newNum
+            mid = len(newNum) // 2
+            left = margeSort(newNum[:mid])
+            right = margeSort(newNum[mid:])
+            
+            return marge(left,right)
+    
+        margeSort(newNum)
+        return res
+
