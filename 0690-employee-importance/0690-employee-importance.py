44597b78dd1085b2566fc22654ee62c9cf83f167
Teklu moges <135549662+teklumt@users.noreply.github.com>
2024-04-03 19:44:49 +0300

    Time: 115 ms (56.57%), Space: 17.8 MB (86.55%) - LeetHub

--- 0690-employee-importance/0690-employee-importance.py
+++ 0690-employee-importance/0690-employee-importance.py
@@ -9,10 +9,26 @@ class Employee:
 
 class Solution:
     def getImportance(self, employees: List['Employee'], id: int) -> int:
-        mydict = {employee.id: employee for employee in employees}
-        def dfs(employee):
-            res=mydict[employee].importance
-            for i in mydict[employee].subordinates:
-                res+=dfs(i)
-            return res
-        return dfs(id)
+        graph = {emp.id :[emp.importance, emp.subordinates] for emp in employees }
+
+        stack = graph[id][-1]
+        res = graph[id][0]
+  
+
+        seen = set()
+        while stack:
+            currId = stack.pop()
+            if currId not in seen:
+                res += graph[currId][0]
+
+            for i in graph[currId][-1]:
+                
+                if i not in seen:
+                    res += graph[i][0]
+                    seen.add(i)
+                    stack+=graph[i][-1]
+        return res
+
+
+
+     
