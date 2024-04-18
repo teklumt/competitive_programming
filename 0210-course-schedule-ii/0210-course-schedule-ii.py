e6881fb4e892e07e4a085b354276c95c9eb0af93
Teklu moges <135549662+teklumt@users.noreply.github.com>
2024-04-18 18:57:09 +0300

    Time: 97 ms (24.83%), Space: 18.4 MB (45.16%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -11,16 +11,13 @@ class Solution:
             if i not in count:
                 queue.append(i)
         res = []
-        seen = set()
         while queue:
             node = queue.popleft()
             res.append(node)
             for i in graph[node]:
-                if count[i] > 0:
-                    count[i] -= 1
-                    if count[i] == 0:
-                        queue.append(i)
-                        seen.add(i)
+                count[i] -= 1
+                if count[i] <= 0:
+                    queue.append(i)
         return res if len(res) == numCourses else []
 
                 
