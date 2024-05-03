fd71c99372762b9a28a9c8a350a29e037b21c3fc
Teklu moges <135549662+teklumt@users.noreply.github.com>
2024-05-04 00:24:24 +0300

    Time: 661 ms (82.51%), Space: 35.9 MB (11.66%) - LeetHub

--- 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.py
+++ 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.py
@@ -5,17 +5,8 @@ class Solution:
             totX ^= nums[i]
         
         res = []
-        bitL = (2**maximumBit).bit_length() - 1
+        maxx = (2**maximumBit)- 1
         for i in nums[::-1]:
-            num = bin(totX)[2:] 
-            if len(num) < bitL:
-                num = "0" * (bitL - len(num)) + num
-            elif len(num) > bitL:
-                num = num[-bitL:]
-            temp = []
-            for j in range(len(num)):
-                temp.append("1" if num[j] == '0' else '0') 
-            res.append(int("".join(temp), 2))
-           
+            res.append(maxx ^ totX)
             totX ^= i
         return res
