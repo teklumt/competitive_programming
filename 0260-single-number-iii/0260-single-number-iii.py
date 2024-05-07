3ac62c07b9fc07bb8bf0cfe7be72854f7421775e
Teklu moges <135549662+teklumt@users.noreply.github.com>
2024-05-07 14:04:19 +0300

    Time: 64 ms (32.68%), Space: 18.4 MB (77.60%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.py
+++ 0260-single-number-iii/0260-single-number-iii.py
@@ -1,16 +1,15 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> List[int]:
-        result=[]
-        check=[]
-        final=[]
-        for n in nums:
-            if n in result:
-                check.append(n)
+        totX = 0
+        for i in nums:
+            totX ^= i 
+        totX &= -totX
+        x, y  = 0, 0
+        for i in nums:
+            if i & totX:
+                x ^= i
             else:
-                result.append(n)
-        for b in result:
-            if b in check:
-                pass
-            else:
-                final.append(b)
-        return final
+                y ^= i 
+
+        return [x, y]
+        
