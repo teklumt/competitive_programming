e4a16f04786d151432c8b25b29bcf3e0998b3b28
Teklu moges <135549662+teklumt@users.noreply.github.com>
2024-05-21 19:28:59 +0300

    Time: 42 ms (71.20%), Space: 16.5 MB (99.23%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -3,25 +3,22 @@ class Solution:
         m , n = len(obstacleGrid), len(obstacleGrid[0])
         if obstacleGrid[m - 1][n - 1] == 1 or obstacleGrid[0][0] == 1: return 0
 
-        direction = [(1, 0), (0, 1)]
-        memo = {}
+        memo =[[0] * n for _ in range(m)]
+        memo[0][0] = 1
+        direction = [(-1, 0), (0, -1)]
+
         def inBound(i , j):
             return 0 <= i < m and 0 <= j < n
-        def func(i , j):
-            if i == m - 1 and j == n - 1:
-                return 1
-            if not inBound(i , j) or obstacleGrid[i][j] == 1:
-                return 0
+        for i in range(m):
+            for j in range(n):
+                if obstacleGrid[i][j] == 1:
+                    memo[i][j] = 0
+                else:
+                
+                    for r , c in direction:
+                        newR, newC = i + r, j + c
+                        if inBound(newR, newC):
+                            memo[i][j] += memo[newR][newC]
 
-            state = (i , j)
-            if state not in memo:
-                memo[state] = 0
-                for r, c in direction:
-                    newR, newC = i + r, j + c
-                    memo[state] += func(newR, newC)
-                   
-            return memo[state] 
 
-        return func(0, 0)
-       
-
+        return memo[-1][-1] 
