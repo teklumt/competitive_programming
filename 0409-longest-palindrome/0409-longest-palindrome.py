730ced1b9ea5be0652ed70dd649f33cbcefe54d5
Teklu moges <135549662+teklumt@users.noreply.github.com>
2024-06-04 15:05:58 +0300

    Time: 34 ms (76.19%), Space: 16.6 MB (80.97%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.py
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -1,33 +1,11 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        count_hash = {}
-        odds = []
-        evens = 0
-
-        for i in s:
-            count_hash[i] = count_hash.get(i,0) + 1
-        
-        for i in count_hash:
-            
-            if count_hash[i]%2==0:
-                evens+=count_hash[i]
-            
-            else:
-                odds.append(count_hash[i])
-        
-        odds.sort()
-
-        for i in range(len(odds)-1,-1,-1):
-
-            if i == len(odds)-1:
-                evens += odds[i]
-            
-            else:
-
-                evens += odds[i]-1
-        return evens
-        
-
-        
-
-        
+        Count = Counter(list(s))
+        res = 0
+        maxx = 0
+        for i in Count:
+            if Count[i] %2 :
+                maxx = 1
+                res += Count[i] - 1
+            else: res += Count[i]
+        return res + (1 if maxx else 0)
