1c640060dba2e3dfb4629aae1d4f816dde02afc6
Teklu moges <135549662+teklumt@users.noreply.github.com>
2024-07-26 17:23:30 +0300

    Time: 13 ms (38.96%), Space: 6.5 MB (5.26%) - LeetHub

---
+++ 1496-lucky-numbers-in-a-matrix/1496-lucky-numbers-in-a-matrix.go
@@ -0,0 +1,51 @@
+func min(arr []int) int {
+    minVal := arr[0]
+    for _, v := range arr {
+        if v < minVal {
+            minVal = v
+        }
+    }
+    return minVal
+}
+
+func max(arr []int) int {
+    maxVal := arr[0]
+    for _, v := range arr {
+        if v > maxVal {
+            maxVal = v
+        }
+    }
+    return maxVal
+}
+func luckyNumbers (matrix [][]int) []int {
+
+    m, n := len(matrix), len(matrix[0])
+
+    var res = []int{}
+    var transponse [][]int = make([][]int, n)
+    for i := range transponse {
+        transponse[i] = make([]int, m)
+    }
+
+    for  i := 0; i < m; i++ {
+        for j := 0; j < n; j++{
+            transponse[j][i] = matrix[i][j]
+
+        }
+    }
+
+    for  i := 0; i < m; i++ {
+        for j := 0; j < n; j++{
+            if matrix[i][j] == min(matrix[i]) && matrix[i][j] == max(transponse[j]){
+                res = append(res, matrix[i][j])
+            }
+
+        }
+    }
+    return res
+
+
+
+
+    
+}
