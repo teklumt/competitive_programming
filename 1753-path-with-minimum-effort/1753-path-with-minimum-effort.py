67c1d63dba5672c9ea215e476273bef28459ef1d
Teklu moges <135549662+teklumt@users.noreply.github.com>
2024-08-10 22:43:04 +0300

    Time: 1096 ms (19.32%), Space: 18.8 MB (37.95%) - LeetHub

--- 1753-path-with-minimum-effort/1753-path-with-minimum-effort.py
+++ 1753-path-with-minimum-effort/1753-path-with-minimum-effort.py
@@ -4,25 +4,27 @@ class Solution:
         def isvalid(i , j):
                 return 0 <= i < len(heights) and 0 <= j < len(heights[0])
 
-        def check(n, i , j, seen):
-            if i == (len(heights) - 1) and j == (len(heights[0]) - 1):
-                return True
+        def check(n):
+            seen = set([(0, 0)])
+            stack = [(0, 0)]
+            while stack:
+                r , c = stack.pop()
+                if r == (len(heights) - 1) and c == (len(heights[0]) - 1):
+                    return True
+                for dx, dy in directions:
+                    newR, newC = r + dx, c + dy
+                    if isvalid(newR, newC) and (newR, newC) not in seen and abs(heights[newR][newC] - heights[r][c]) <= n:
+                        seen.add((newR, newC))
+                        stack.append((newR, newC))
+            return False 
             
-            for r, c in directions:
-                newR,  newC = i + r, j + c
-                if isvalid(newR, newC) and ((newR, newC) not in seen) and (abs(heights[newR][newC] - heights[i][j]) <= n):
-                    seen.add((newR,newC))
-                    if check(n, newR, newC, seen):
-                        return True
-            return False
             
 
         left , right = 0 , 10 ** 6 
         res = inf
         while left <= right:
             mid = (left + right) // 2
-            ch = check(mid, 0, 0, set([(0 , 0)]))
-            if ch:
+            if check(mid):
                 res = min(res, mid)
                 right = mid - 1
             else:
