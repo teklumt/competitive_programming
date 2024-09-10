53910a79124a4359e8a6b2c27ffc3a70467a7c86
Teklu moges <135549662+teklumt@users.noreply.github.com>
2024-09-10 13:37:39 +0300

    Time: 1811 ms (27.51%), Space: 16.5 MB (62.08%) - LeetHub

---
+++ 2541-sum-of-number-and-its-reverse/2541-sum-of-number-and-its-reverse.py
@@ -0,0 +1,10 @@
+class Solution:
+    def sumOfNumberAndReverse(self, num: int) -> bool:
+        if num == 0:return True
+        for i in range(num):
+            sec = num - i
+            two = int(str(i)[::-1])
+            if sec >= 0 and two >= 0 and sec == two:
+                return True
+        return False
+            
