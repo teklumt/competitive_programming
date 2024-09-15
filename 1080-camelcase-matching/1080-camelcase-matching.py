7c9d33f8cdf8d667127fc52c0bc96f8775e4e30a
Teklu moges <135549662+teklumt@users.noreply.github.com>
2024-09-15 15:30:58 +0300

    Time: 40 ms (25.43%), Space: 16.6 MB (47.68%) - LeetHub

--- 1080-camelcase-matching/1080-camelcase-matching.py
+++ 1080-camelcase-matching/1080-camelcase-matching.py
@@ -1,22 +1,36 @@
+class Trie:
+    def __init__(self):
+        self.root  = {"#":0}
+
+    def insert(self, pattern):
+        cur = self.root
+        for i in pattern:
+            if i not in cur:
+                cur[i] = {"#" : 0}
+            cur = cur[i]
+        cur["#"] = 1
+        
+    def match(self, given):
+        cur = self.root
+        i = 0
+        while i < len(given):
+            
+            curChar = given[i]
+            if curChar.isupper():
+                if curChar not in cur:
+                    return False
+                cur = cur[curChar]
+
+            elif curChar in cur:
+                cur = cur[curChar]
+            i += 1
+        return cur["#"] == 1
+
 class Solution:
     def camelMatch(self, queries: List[str], pattern: str) -> List[bool]:
+        tri = Trie()
+        tri.insert(pattern)
         res = []
         for i in queries:
-            l = 0
-            bol = True
-            for j in range(len(i)):
-                if  l < len(pattern) and pattern[l] == i[j] :
-                    l += 1
-                elif ord(i[j]) < 97:
-                    res.append(False)
-                    bol = False
-                    break
-                # if l == len(pattern):break
-            if bol:
-                if l == len(pattern):
-                    res.append(True)
-                else:
-                    res.append(False)
-
+            res.append(tri.match(i))
         return res
-            
