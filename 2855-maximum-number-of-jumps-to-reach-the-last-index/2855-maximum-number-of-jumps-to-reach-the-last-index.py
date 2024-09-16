6d21b077595e91fa0394231d78ec5e2de4c4e748
Teklu moges <135549662+teklumt@users.noreply.github.com>
2024-09-16 16:32:09 +0300

    Time: 503 ms (84.68%), Space: 16.8 MB (37.1%) - LeetHub

--- 2855-maximum-number-of-jumps-to-reach-the-last-index/2855-maximum-number-of-jumps-to-reach-the-last-index.py
+++ 2855-maximum-number-of-jumps-to-reach-the-last-index/2855-maximum-number-of-jumps-to-reach-the-last-index.py
@@ -1,11 +1,12 @@
 class Solution:
     def maximumJumps(self, nums: List[int], target: int) -> int:
-        dp = [0] * len(nums)
+        dp = [-1] * len(nums)
+        dp[0] = 0
         for i in range(len(nums)):
-            if i == 0 or dp[i] != 0:
+            if dp[i] != -1:
                 for j in range(i + 1, len(nums)):
                     if abs(nums[i] - nums[j]) <= target:
                         bol = False
                         dp[j] = max(dp[j], dp[i] + 1)
                 
-        return dp[-1] if dp[-1] else -1
+        return dp[-1] 
