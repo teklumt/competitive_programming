d3c33e255421eb812d5532b3d5ba667c1f465ee0
Teklu moges <135549662+teklumt@users.noreply.github.com>
2024-10-05 14:26:42 +0300

    Time: 55 ms (38.05%), Space: 17 MB (74.09%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
@@ -1,25 +1,26 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> bool:
-        left = 0
-        right = len(nums) - 1
-        while left <= right:
-            mid = (left + right ) // 2
-
-            if nums[mid] == target:
-                return True
-                
-            if nums[left] == nums[mid] == nums[right]:
+        left, right = 0, len(nums) - 1
+        def func():
+            nonlocal left, right
+            while left < right and nums[left] == nums[right]:
                 left += 1
-                right -= 1
-            elif nums[left] <= nums[mid]:
-                if nums[left] <=   target < nums[mid]:
+        func()
+        
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target or nums[left] == target or nums[right] == target:
+                return True
+
+            if nums[left] <= nums[mid]:
+                if nums[left] <= target < nums[mid]:
                     right = mid - 1
                 else:
                     left = mid + 1
             else:
                 if nums[mid] < target <= nums[right]:
-                    left = mid + 1
+                    left = mid  + 1
                 else:
                     right = mid - 1
+            func()
         return False
-                
